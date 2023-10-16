2fbd253e989a4b884d4552fd18c5785efc334e3b
yassinechabla <yassinechabla@gmail.com>
2023-10-17 01:46:01 +0200

    update Bonjour.java

--- TME1/Bonjour.java
+++ TME1/Bonjour.java
@@ -1,7 +1,7 @@
-package TME1;
+package JAVA.TME1;
 
 public class Bonjour {
      public static void main(String [] args){
-        System.out.println("Bonjour !");
+        System.out.println("Bonjour Yassine !");
     }
 }
